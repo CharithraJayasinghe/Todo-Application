90eac95e0f1ff508999eb74f318e440f671255e3
“Charithra” <charithra@adtechnologies.lk>
2024-06-09 20:13:59 +0530

    todo app

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,34 @@
+import styles from "./todoitem.module.css";
+export default function TodosList({ item, todos, setTodos }) {
+  function handleDelete(item) {
+    console.log("delete ", item);
+    setTodos(todos.filter((todo) => todo !== item));
+  }
+  function handleClick(name) {
+    setTodos(
+      todos.map((todo) =>
+        todo.name === name ? { ...todo, done: !todo.done } : todo
+      )
+    );
+    console.log(todos);
+  }
+  const className = item.done ? styles.completed : "";
+  return (
+    <div className={styles.item}>
+      <div className={styles.itemName}>
+        <span className={className} onClick={() => handleClick(item.name)}>
+          {item.name}
+        </span>
+        <span>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deleteButton}
+          >
+            x
+          </button>
+        </span>
+      </div>
+      <hr className={styles.line} />
+    </div>
+  );
+}
